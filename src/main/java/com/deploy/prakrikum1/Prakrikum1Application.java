dff7008ca6f19e420b1a14e13416e8ea95174bce
HaniefuL <haniefulgeofarry@gmail.com>
2026-03-02 10:19:25 +0700

    Initial commit

---
+++ src/main/java/com/deploy/prakrikum1/Prakrikum1Application.java
@@ -0,0 +1,13 @@
+package com.deploy.prakrikum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prakrikum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prakrikum1Application.class, args);
+	}
+
+}
